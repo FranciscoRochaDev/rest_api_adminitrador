8cf24947b1e003dcfb3af25d65358f3dde08fe7f
Francisco Rocha <fa676533@gmail.com>
2026-08-03 14:21:01 -0600

    feat: Implementar CORS, logging con Morgan y ajustes en el endpoint de productos

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import router from "./router"
 import db from "./config/db"
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 
 // Conection data base
 export async function connectDB() {
@@ -23,8 +25,23 @@ connectDB()
 // Instacia de Express
 const server = express()
 
+// Permitir conexiones
+const cordsOptions : CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL){
+            callback(null,true)
+        }else{
+            callback(new Error("Error en CORS"))
+        }
+    }
+}
+
+server.use(cors(cordsOptions))
+
 // Leer Datos de Formularios
 server.use(express.json())
+
+server.use(morgan("dev"))
 
 // URL DE LAS RUTAS
 server.use('/api/products', router)
